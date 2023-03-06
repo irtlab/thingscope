5bbeba3999f9631a5bd37045f74efcc6860ec99a
Andrew Schwartz <AJSchwartz23@gmail.com>
2023-03-06 15:46:42 -0500

    Support to pull files from google drive

--- 23-Spring/agent/aws_handler.py
+++ 23-Spring/agent/aws_handler.py
@@ -24,36 +24,53 @@ class s3():
 	def put(self, key, file):
 		self.s3Client.put_object(Bucket = self.bucket, Key=key, Body=file, ContentDisposition='inline')
 
-def googlePush(filename, file):
-	filepath = f"/tmp/{filename}"
-	with open(filepath, 'w') as f:
-		f.write(file)
-	secret_name = "thingscopegoogle"
-	region_name = "us-east-1"
+class google():
+	def __init__(self):
+		secret_name = "thingscopegoogle"
+		region_name = "us-east-1"
+		session = boto3.session.Session()
+		client = session.client(service_name='secretsmanager',region_name=region_name)
+		get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+		creds = json.loads(get_secret_value_response['SecretString'])["thingscopegoogle"]
+		credentials = Credentials.from_service_account_info(creds)
+		self.drive = build('drive', 'v3', credentials=credentials)
 
-	session = boto3.session.Session()
-	client = session.client(service_name='secretsmanager',region_name=region_name)
-	get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-	creds = json.loads(get_secret_value_response['SecretString'])["thingscopegoogle"]
 
-	credentials = Credentials.from_service_account_info(creds)
-	drive = build('drive', 'v3', credentials=credentials)
-	file_metadata = {'name': filename,'mimeType': 'text/plain', 'parents': ['1A0ScUpFS6B9nbOxGo0GlSuHwjRpDx060']}
-	media = MediaFileUpload(mimetype='text/plain', filename=filepath)
+	def push(self, filename, file):
+		filepath = f"/tmp/{filename}"
+		with open(filepath, 'w') as f:
+			f.write(file)
+		file_metadata = {'name': filename,'mimeType': 'text/plain', 'parents': ['1A0ScUpFS6B9nbOxGo0GlSuHwjRpDx060']}
+		media = MediaFileUpload(mimetype='text/plain', filename=filepath)
+		file = self.drive.files().create(body=file_metadata, media_body=media).execute()
 
-	file = drive.files().create(body=file_metadata, media_body=media).execute()
+	def get(self, key):
+		filename = self.drive.files().get(fileId=key).execute()['name']
+		path = f"/tmp/{filename}"
+		file = self.drive.files().get_media(fileId=key).execute()
+		with open(path, 'wb') as f:
+			f.write(file)
+		return filename
 
-def handler(event, context):
-	time.sleep(30)
+def main(pcap_file):
 	global log_array
 	log_array = []
 	logger = logging.getLogger()
 	logger.setLevel(logging.INFO)
 	logger.addHandler(ArrayHandler())
-	key = event['Records'][0]['s3']['object']['key'] 
+	
+	process_pcap(f"/tmp/{pcap_file}", ["10.", "192.168.", "253.", "254.", "255."])
+	filename = f"report_{pcap_file.split('.')[:-1][0]}.txt"
+	s3().put(filename, '\n'.join(log_array))
+	google().push(filename, '\n'.join(log_array))
+
+def handlerFromS3(event, context):
+	key = event['Records'][0]['s3']['object']['key']
 	if key.split('.')[-1] == 'pcap':
 		pcap_file = s3().get(key)
-		process_pcap(pcap_file, ["10.", "192.168.", "253.", "254.", "255."])
-		filename = f"report_{key.split('.')[:-1][0]}.txt"
-		s3().put(filename, '\n'.join(log_array))
-		googlePush(filename, '\n'.join(log_array))
+		main(pcap_file)
+
+def handlerFromDrive(event, context):
+	filename = google().get(event['rawQueryString'])
+	main(filename)
+	return {'status':202}
