2f90f6c1bf22a51234697184120f3e0ebc1fa1cd
Andrew Schwartz <AJSchwartz23@gmail.com>
2023-03-27 19:24:24 -0400

    Rename lambda template, pre-docker commit

--- 23-Spring/agent/scripts/aws_deploy.sh
+++ 23-Spring/agent/scripts/aws_deploy.sh
@@ -1,4 +1,4 @@
 mv requirements.txt requirements_temp.txt
-sam build --skip-pull-image
+sam build --skip-pull-image -t lambda_template.yaml
 sam deploy --no-progressbar
 mv requirements_temp.txt requirements.txt
