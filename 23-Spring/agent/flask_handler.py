9f0c203118e1ab8b36003ec58167ad5352ebd14e
Andrew Schwartz <AJSchwartz23@gmail.com>
2023-04-16 10:48:09 -0400

    flask cleanup

--- 23-Spring/agent/flask_handler.py
+++ 23-Spring/agent/flask_handler.py
@@ -4,6 +4,7 @@ from aws_handler import *
 import json
 import os
 from sink import *
+from compare import *
 
 app = Flask(__name__)
 
@@ -11,18 +12,17 @@ app = Flask(__name__)
 # The saved file should be in the attached volume in the docker compose file
 @app.route("/agent")
 def agent():
-    #try:
+    try:
         path = request.args.get('path')
         title = request.args.get('title')
-        process_pcap_sink(f"pcap/{path}", title, os.environ['ATLAS_URI'], os.environ['IOT_DB_NAME'])
+        process_pcap_sink(f"../data/files/{path}", title, os.environ['ATLAS_URI'], os.environ['IOT_DB_NAME'])
         return 'Sucess', 200
-    # except Exception as e:
-    #     print(e)
-    #     return 'Internal Server Error', 500
+    except Exception as e:
+         print(e)
+         return 'Internal Server Error', 500
     
 @app.route("/agent/legacy")
 def agentLegacy():
-    return handlerFromDrive({'queryStringParameters' : {'id': request.args.get('id')}}, None)
     try:
         return handlerFromDrive({'queryStringParameters' : {'id': request.args.get('id')}}, None)
     except Exception as e:
@@ -38,13 +38,12 @@ def compare():
     #    return str(e), 500
 
 # Uploads a file to the volume. Posts the file in the HTTP request
-@app.route("/upload_pcap", methods=['POST'])
+@app.route("/agent/upload_pcap", methods=['POST'])
 def upload():
     try:
-        if request.method == 'POST':
-            f = request.files['file']
-            f.save(f"pcap/{f.filename}")
-            return '200 OK', 200
+        f = request.files['file']
+        f.save(f"pcap/{f.filename}")
+        return '200 OK', 200
     except Exception as e:
         print(e)
         return 'Internal Server Error', 500
@@ -53,19 +52,19 @@ def upload():
 @app.route("/agent/list_pcap")
 def list_pcap():
     #try:
-        files = os.listdir("pcap")
-        return '<br>'.join(files), 200
+    files = os.listdir("pcap")
+    return '<br>'.join(files), 200
     # except Exception as e:
     #     print(e)
     #     return 'Internal Server Error', 500
 
-@app.route("/agent/output_db/endpoints")
+@app.route("/agent/db/endpoints")
 def output_db_endpoints():
     db = DeviceSink(db_url=os.environ['ATLAS_URI'], db_name=os.environ['IOT_DB_NAME'])
     endpoints = [x for x in db.endpoints_coll.find()]
     return endpoints, 200
 
-@app.route("/agent/output_db/devices")
+@app.route("/agent/db/devices")
 def output_db_devices():
     db = DeviceSink(db_url=os.environ['ATLAS_URI'], db_name=os.environ['IOT_DB_NAME'])
     devices = [x for x in db.devices_coll.find()]
@@ -73,7 +72,7 @@ def output_db_devices():
         device['_id'] = str(device['_id'])
     return devices, 200
 
-@app.route("/agent/output_db/domains")
+@app.route("/agent/db/domains")
 def output_db_domains():
     db = DeviceSink(db_url=os.environ['ATLAS_URI'], db_name=os.environ['IOT_DB_NAME'])
     domains = [x for x in db.domain_coll.find()]
