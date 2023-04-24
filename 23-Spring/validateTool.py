67c502bdf673cee3ffe1109bbf5bb4f70a603dc7
Andrew Schwartz <AJSchwartz23@gmail.com>
2023-04-24 10:45:57 -0400

    Create validateTool.py
    Created validateTool, which checks that the adjustments to the device security scanner do not change the endpoint outputs. It does this by comparing the endpoints.json from the old data to the output of the new system

---
+++ 23-Spring/validateTool.py
@@ -0,0 +1,27 @@
+# compare the IPs in endpoints.json to the IPs in mongodb
+import json
+import os
+from agent.sink import *
+import ipaddress
+
+# os.walk the pankaj/data folder. For each folder, the name of the device is the name of the folder and the ips are the endpoints.json file inside that folder
+sink = DeviceSink()
+
+for root, dirs, files in os.walk("../../pankaj/data"):
+    for name in dirs:
+        deviceName = name.replace("_", ":")
+        print(deviceName)
+        with open(os.path.join(root, name, "endpoints.json")) as f:
+            data = json.load(f)
+            for endpoint in data:
+                ip = endpoint['ip']
+                if ipaddress.ip_address(ip).is_private:
+                    continue
+                # check if the ip exists in the mongodb
+                # if it does, then check if the ip is in the same device
+                tempEndpoints = [x for x in sink.endpoints_coll.find({'ip': ip, 'device_mac': deviceName})]
+                if tempEndpoints:
+                    pass
+                    #print(f"\t{ip} exists in {deviceName}")
+                else:
+                    print(f"\t{ip} does not exist in {deviceName}")
